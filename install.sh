e41b55eb0e5a995889a90d3b39f71c15a48947e6
WildCommunist <alex@ceres.ventures>
2025-02-06 12:49:23 +0500

    Initial release

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ docker() {
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
   docker version
 
   sudo usermod -aG docker ${USER}
